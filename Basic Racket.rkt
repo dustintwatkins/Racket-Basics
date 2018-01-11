882ea193f0bc6e466a75562dd72ea17d85a5c9c5
dustintwatkins <dustintwatkins@gmail.com>
2018-01-10 22:26:10 -0700

    finished

---
+++ Basic Racket.rkt
@@ -0,0 +1,33 @@
+#lang racket
+
+(provide (all-defined-out))
+
+;Sum Coins
+
+(define (sum-coins p n d q)
+  (+ (* .1 d)(+ (* .05 n)(+ (* .01 p)(* .25 q)))))
+
+;Degrees to Radians
+
+(define (degrees-to-radians angle)
+  (* pi (/ angle 180)))
+
+;sign
+(define (sign x)
+  (cond
+    ((= 0 x) x)
+    ((positive? x) 1)
+    (else -1)
+   )   ; end condition
+ )
+
+;new-sin
+(define (new-sin angle type)
+  (cond
+    ((symbol=? type 'degrees)
+     (sin (degrees-to-radians angle)))
+    ((symbol=? type 'radians)
+     (sin angle))
+    (else "not a valid type")
+  ) ; end condition
+)
